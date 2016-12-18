khadas/linux

ARM:meson64: add gxbaby support
Change-Id: I1703ddac27bd8ba68c16bf22d69dd83bdcbf4c96

drivers/base/cpu.c
@@ -314,6 +314,7 @@ static ssize_t print_cpu_modalias(struct device *dev,
 #define print_cpu_modalias	arch_print_cpu_modalias
 #endif
 
+#ifdef CONFIG_ARCH_HAS_CPU_AUTOPROBE
 static int cpu_uevent(struct device *dev, struct kobj_uevent_env *env)
 {
 	char *buf = kzalloc(PAGE_SIZE, GFP_KERNEL);
@@ -324,6 +325,7 @@ static int cpu_uevent(struct device *dev, struct kobj_uevent_env *env)
 	}
 	return 0;
 }
+#endif
 #endif
 
 /*

Kwiboo/linux-amlogic

drivers/base/cpu.c
@@ -314,6 +314,7 @@ static ssize_t print_cpu_modalias(struct device *dev,
 #define print_cpu_modalias	arch_print_cpu_modalias
 #endif
 
+#ifdef CONFIG_HAVE_CPU_AUTOPROBE
 static int cpu_uevent(struct device *dev, struct kobj_uevent_env *env)
 {
 	char *buf = kzalloc(PAGE_SIZE, GFP_KERNEL);
@@ -324,6 +325,7 @@ static int cpu_uevent(struct device *dev, struct kobj_uevent_env *env)
 	}
 	return 0;
 }
+#endif
 #endif
 
 /*